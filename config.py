6886d8debab661d87c40776e56f9d24865f55bf0
MineCoder <84143634+miravaseva9@users.noreply.github.com>
2021-05-15 18:59:48 -0700

    1.0.9.5

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 from python_anticaptcha import AnticaptchaClient
 
-CAPTCHA_KEY = 'YOUR KEY'  # https://anti-captcha.com/
+CAPTCHA_KEY = 'ce58a608a8fb5ffcb092eb17fa4bcf55'  # https://anti-captcha.com/
 # PROXY_KEY = ''  # https://best-proxies.ru/
 
 anticaptcha_client = AnticaptchaClient(CAPTCHA_KEY)
@@ -10,14 +10,12 @@ anticaptcha_client = AnticaptchaClient(CAPTCHA_KEY)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/90.0.4430.85 Safari/537.36'}
 
-WORK_SITE_DIR = f'file:///{os.getcwd()}//js//index.html'  # website creation with WAX API
+WORK_SITE_DIR = f'file:///{os.getcwd()}//js//index.html'  # создание сайта с API WAX
 
-headless = True  
-
-
-# do not forget to put cookies in the Cookies folder
+headless = False  # скрыто ли окно браузера
 
 
+# не забываем что куки надо положить в папку /Cookies
 
 
 
@@ -70,5 +68,4 @@ headless = True
 
 
 
-
-MAIN_ACCOUNT = 'mlxtk.wam' # a smart contract account for the bot. Do not change.
+MAIN_ACCOUNT = 'tq4si.wam' # аккаунт смарт контракта для работы бота. НЕ менять.
